86bf835cb5586de0a71b7fd67b94f33034135773
FlailoftheLord <jeddyh15@gmail.com>
2018-11-28 23:32:41 -0500

    added Economy triggers
    finishing up the cook command, added some economy triggers, screwed with placeholders... ect.

--- src/me/flail/Toaster/Cook/CookCommand.java
+++ src/me/flail/Toaster/Cook/CookCommand.java
@@ -2,10 +2,14 @@ package me.flail.Toaster.Cook;
 
 import java.util.Set;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import me.flail.Toaster.Toaster;
 import me.flail.Toaster.Tools;
@@ -41,6 +45,18 @@ public class CookCommand {
 
 						Set<String> items = itemList.getKeys(false);
 
+						ItemStack pHand = player.getInventory().getItemInMainHand();
+
+						String pItemName = pHand.getType().toString();
+
+						double playerBalance = eco.getBalance(player);
+
+						double maxWithdraw = config.getDouble("MaxMoneyWithdraw");
+
+						int pItemAmount = pHand.getAmount();
+
+						boolean validItem = true;
+
 						for (String item : items) {
 
 							String rawItem = itemList.getString(item + ".Item").toUpperCase();
@@ -51,18 +67,112 @@ public class CookCommand {
 
 							double cost = itemList.getDouble(item + ".Price");
 
-							double exp = 0.0;
+							String cookSuccess = tools.chat(
+									config.getString("Cook.Success").replaceAll("<item>", item)
+											.replaceAll("<result>", cookedItem).replaceAll("<amount>", pItemAmount + "")
+											.replaceAll("<cost>", (cost * pItemAmount) + "")
+											.replaceAll("<price>", cost + "").replaceAll("<exp>", expVariable),
+									command, player);
 
-							if (expVariable.endsWith("L")) {
+							String cookNoMoney = tools.chat(config.getString("Cook.DontHaveEnoughMoney")
+									.replaceAll("<item>", item).replaceAll("<result>", cookedItem)
+									.replaceAll("<amount>", pItemAmount + "").replaceAll("<exp>", expVariable), command,
+									player);
+
+							if (pItemName.equalsIgnoreCase(rawItem)) {
+
+								ItemStack cookedResult = new ItemStack(Material.getMaterial(cookedItem), pItemAmount);
+
+								double totalCost = cost * pItemAmount;
+
+								if (!(totalCost > playerBalance)) {
+
+									if (!(totalCost > maxWithdraw)) {
+
+										if (expVariable.endsWith("L")) {
+											int exp = Integer.parseInt(expVariable.replace("L", ""));
+											player.giveExpLevels(exp * pItemAmount);
+										} else {
+											int exp = Integer.parseInt(expVariable);
+											player.giveExp(exp * pItemAmount);
+										}
+
+										eco.withdrawPlayer(player, totalCost);
+
+										player.getInventory().setItemInMainHand(cookedResult);
+
+										player.playSound(player.getLocation(), Sound.BLOCK_FURNACE_FIRE_CRACKLE, 2, 2);
+
+										player.sendMessage(cookSuccess);
+
+										validItem = true;
+										break;
+									} else {
+
+										String cantSpendThatMuch = tools.chat(config.getString("CannotSpend")
+												.replaceAll("<item>", item).replaceAll("<result>", cookedItem)
+												.replaceAll("<amount>", pItemAmount + "")
+												.replaceAll("<cost>", (totalCost) + "").replaceAll("<price>", cost + "")
+												.replaceAll("<exp>", expVariable)
+												.replaceAll("<maxWithdraw>", maxWithdraw + ""), command, player);
+
+										player.sendMessage(cantSpendThatMuch);
+
+										validItem = true;
+										break;
+
+									}
+
+								} else {
+
+									player.sendMessage(cookNoMoney.replaceAll("<cost>", totalCost + "")
+											.replaceAll("<price>", totalCost + ""));
+
+									break;
+								}
+
+							} else {
+								validItem = false;
 
 							}
 
+						}
+
+						if (!validItem) {
+							String cantCook = tools.chat(config.getString("Cook.InvalidItem")
+									.replaceAll("<item>", pItemName).replaceAll("<amount>", pItemAmount + ""), command,
+									player);
+
+							player.sendMessage(cantCook);
 						}
 
 					}
 
 				} else {
 					player.sendMessage(tools.chat(noPermission, command, player));
+				}
+
+			} else if (args.length == 1) {
+
+				String subject = args[0];
+
+				if (subject.equalsIgnoreCase("items")) {
+
+				} else if (subject.equalsIgnoreCase("help")) {
+
+				} else {
+
+					for (Player p : Bukkit.getOnlinePlayers()) {
+
+						String pName = p.getName();
+
+						if (subject.equalsIgnoreCase(pName)) {
+
+							break;
+						}
+
+					}
+
 				}
 
 			}
--- src/me/flail/Toaster/ToasterCommand.java
+++ src/me/flail/Toaster/ToasterCommand.java
@@ -1,5 +1,11 @@
 package me.flail.Toaster;
 
+import org.bukkit.command.CommandSender;
+
 public class ToasterCommand {
+
+	public void toasterCommand(CommandSender sender, String command, String[] args) {
+
+	}
 
 }
